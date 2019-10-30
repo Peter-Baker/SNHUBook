b2aafb44ca49c76262a9eb2f4e87488479ece4bb
CaelaMc1 <caela.mccartney@snhu.edu>
2019-10-29 21:59:28 -0400

    Button Functions updated
    Added function to clicking "UserName" "Photo/Video", and "Home"

--- WindowsFormsApp7/Form2.Designer.cs
+++ WindowsFormsApp7/Form2.Designer.cs
@@ -33,6 +33,7 @@
             this.SNHUbook_lbl = new System.Windows.Forms.Label();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.Read_File_bn = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
             this.SNHUbook.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -64,28 +65,44 @@
             // 
             this.richTextBox1.Location = new System.Drawing.Point(0, 52);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(581, 185);
+            this.richTextBox1.Size = new System.Drawing.Size(581, 317);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
             // Read_File_bn
             // 
             this.Read_File_bn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Read_File_bn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.Read_File_bn.Font = new System.Drawing.Font("Franklin Gothic Medium", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Read_File_bn.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.Read_File_bn.Location = new System.Drawing.Point(32, 253);
+            this.Read_File_bn.Location = new System.Drawing.Point(45, 391);
             this.Read_File_bn.Name = "Read_File_bn";
             this.Read_File_bn.Size = new System.Drawing.Size(90, 33);
             this.Read_File_bn.TabIndex = 2;
-            this.Read_File_bn.Text = "Read File";
+            this.Read_File_bn.Text = "Browse...";
             this.Read_File_bn.UseVisualStyleBackColor = false;
             this.Read_File_bn.Click += new System.EventHandler(this.Read_File_bn_Click);
+            // 
+            // Add
+            // 
+            this.Add.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Add.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Add.Font = new System.Drawing.Font("Franklin Gothic Medium", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Add.Location = new System.Drawing.Point(448, 391);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(90, 33);
+            this.Add.TabIndex = 3;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = false;
             // 
             // PhotoPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(582, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(582, 436);
+            this.Controls.Add(this.Add);
             this.Controls.Add(this.Read_File_bn);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.SNHUbook);
@@ -104,5 +121,6 @@
         private System.Windows.Forms.Label SNHUbook_lbl;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button Read_File_bn;
+        private System.Windows.Forms.Button Add;
     }
 }
--- WindowsFormsApp7/Form2.cs
+++ WindowsFormsApp7/Form2.cs
@@ -20,10 +20,48 @@ namespace WindowsFormsApp7
 
         private void Read_File_bn_Click(object sender, EventArgs e)
         {
-            openFileDialog1.ShowDialog();
+            /*openFileDialog1.ShowDialog();
             string filename = openFileDialog1.FileName;
             string readfile = File.ReadAllText(filename);
-            richTextBox1.Text = readfile;
+            richTextBox1.Text = readfile;*/
+
+            this.openFileDialog1.Filter =
+"Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF|" +
+"All files (*.*)|*.*";
+
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "Select Photos";
+
+            DialogResult dr = this.openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach (String file in openFileDialog1.FileNames)
+                {
+                    try
+                    {
+                        PictureBox imageControl = new PictureBox();
+                        imageControl.Height = 400;
+                        imageControl.Width = 400;
+
+                        Image.GetThumbnailImageAbort myCallback =
+                                new Image.GetThumbnailImageAbort(ThumbnailCallback);
+                        Bitmap myBitmap = new Bitmap(file);
+                        Image myThumbnail = myBitmap.GetThumbnailImage(300, 300,
+                            myCallback, IntPtr.Zero);
+                        imageControl.Image = myThumbnail;
+
+                        richTextBox1.Controls.Add(imageControl);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                }
+            }
         }
+        public bool ThumbnailCallback()
+            {
+                return false;
+            }
     }
 }
