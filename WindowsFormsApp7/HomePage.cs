9b55ed042bd4d0fccb3b5c03929ddada51c531cc
PeterBaker0213 <peter.baker@snhu.edu>
2019-11-04 11:50:43 -0500

    Successful build, but nothing closes

--- WindowsFormsApp7/HomePage.cs
+++ WindowsFormsApp7/HomePage.cs
@@ -60,7 +60,6 @@ namespace WindowsFormsApp7
             }
         }
 
-        private void panel4_Paint(object sender, PaintEventArgs e)
         private void Check_In_Click(object sender, EventArgs e)
         {
 
