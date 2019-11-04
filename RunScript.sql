d9753c6da24a2322233ddad16b553cf15c2a8b98
PeterBaker0213 <peter.baker@snhu.edu>
2019-11-04 15:59:32 -0500

    Update RunScript.sql

--- RunScript.sql
+++ RunScript.sql
@@ -2,5 +2,5 @@ create database SNHUBook;
 
 use SNHUBook;
 
-create table Accounts(ID mediumint not null auto_increment, FirstName varchar(30) not null, LastName varchar(30) not null, Email varchar(100) not null, Password varchar(50) not null, profImage_Path varchar(1024) not null, backgroundImage_path(1024) not null, Primary Key(ID));
+create table Accounts(ID mediumint not null auto_increment, FirstName varchar(30) not null, LastName varchar(30) not null, Email varchar(100) not null, Password varchar(50) not null, profImage_Path varchar(1024) not null, backgroundImage_path varchar(1024) not null, Primary Key(ID));
 
