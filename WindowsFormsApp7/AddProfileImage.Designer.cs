2443bdd1e1ddae9ae33b25623a13a16cc367c809
Jacob Burton <jacob.burton@snhu.edu>
2019-11-25 18:24:40 -0800

    :)

--- WindowsFormsApp7/AddProfileImage.Designer.cs
+++ WindowsFormsApp7/AddProfileImage.Designer.cs
@@ -44,20 +44,18 @@
             // 
             this.selectImageText.AutoSize = true;
             this.selectImageText.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.selectImageText.Location = new System.Drawing.Point(784, 186);
-            this.selectImageText.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.selectImageText.Location = new System.Drawing.Point(294, 78);
             this.selectImageText.Name = "selectImageText";
-            this.selectImageText.Size = new System.Drawing.Size(766, 78);
+            this.selectImageText.Size = new System.Drawing.Size(320, 31);
             this.selectImageText.TabIndex = 0;
             this.selectImageText.Text = "Select an Image Below:";
             // 
             // CancelButtonProfilePhoto
             // 
             this.CancelButtonProfilePhoto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CancelButtonProfilePhoto.Location = new System.Drawing.Point(1027, 749);
-            this.CancelButtonProfilePhoto.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.CancelButtonProfilePhoto.Location = new System.Drawing.Point(385, 314);
             this.CancelButtonProfilePhoto.Name = "CancelButtonProfilePhoto";
-            this.CancelButtonProfilePhoto.Size = new System.Drawing.Size(325, 117);
+            this.CancelButtonProfilePhoto.Size = new System.Drawing.Size(122, 49);
             this.CancelButtonProfilePhoto.TabIndex = 5;
             this.CancelButtonProfilePhoto.Text = "Cancel";
             this.CancelButtonProfilePhoto.UseVisualStyleBackColor = true;
@@ -66,10 +64,9 @@
             // profileImageFour
             // 
             this.profileImageFour.Image = global::WindowsFormsApp7.Properties.Resources.FemaleAvatar2;
-            this.profileImageFour.Location = new System.Drawing.Point(1896, 329);
-            this.profileImageFour.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.profileImageFour.Location = new System.Drawing.Point(711, 138);
             this.profileImageFour.Name = "profileImageFour";
-            this.profileImageFour.Size = new System.Drawing.Size(373, 334);
+            this.profileImageFour.Size = new System.Drawing.Size(140, 140);
             this.profileImageFour.TabIndex = 4;
             this.profileImageFour.TabStop = false;
             this.profileImageFour.Click += new System.EventHandler(this.profileImageFour_Click);
@@ -77,10 +74,9 @@
             // profileImageThree
             // 
             this.profileImageThree.Image = global::WindowsFormsApp7.Properties.Resources.FemaleAvatar1;
-            this.profileImageThree.Location = new System.Drawing.Point(1301, 329);
-            this.profileImageThree.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.profileImageThree.Location = new System.Drawing.Point(488, 138);
             this.profileImageThree.Name = "profileImageThree";
-            this.profileImageThree.Size = new System.Drawing.Size(373, 334);
+            this.profileImageThree.Size = new System.Drawing.Size(140, 140);
             this.profileImageThree.TabIndex = 3;
             this.profileImageThree.TabStop = false;
             this.profileImageThree.Click += new System.EventHandler(this.profileImageThree_Click);
@@ -88,10 +84,9 @@
             // profileImageTwo
             // 
             this.profileImageTwo.Image = global::WindowsFormsApp7.Properties.Resources.MaleAvatar2;
-            this.profileImageTwo.Location = new System.Drawing.Point(728, 329);
-            this.profileImageTwo.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.profileImageTwo.Location = new System.Drawing.Point(273, 138);
             this.profileImageTwo.Name = "profileImageTwo";
-            this.profileImageTwo.Size = new System.Drawing.Size(373, 334);
+            this.profileImageTwo.Size = new System.Drawing.Size(140, 140);
             this.profileImageTwo.TabIndex = 2;
             this.profileImageTwo.TabStop = false;
             this.profileImageTwo.Click += new System.EventHandler(this.profileImageTwo_Click);
@@ -99,29 +94,27 @@
             // profileImageOne
             // 
             this.profileImageOne.Image = global::WindowsFormsApp7.Properties.Resources.MaleAvatar1;
-            this.profileImageOne.Location = new System.Drawing.Point(200, 329);
-            this.profileImageOne.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.profileImageOne.Location = new System.Drawing.Point(75, 138);
             this.profileImageOne.Name = "profileImageOne";
-            this.profileImageOne.Size = new System.Drawing.Size(373, 334);
+            this.profileImageOne.Size = new System.Drawing.Size(140, 140);
             this.profileImageOne.TabIndex = 1;
             this.profileImageOne.TabStop = false;
             this.profileImageOne.Click += new System.EventHandler(this.profileImageOne_Click);
             // 
             // AddProfileImage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.CornflowerBlue;
             this.BackgroundImage = global::WindowsFormsApp7.Properties.Resources.blue_gradient_background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(2491, 1171);
+            this.ClientSize = new System.Drawing.Size(934, 408);
             this.Controls.Add(this.CancelButtonProfilePhoto);
             this.Controls.Add(this.profileImageFour);
             this.Controls.Add(this.profileImageThree);
             this.Controls.Add(this.profileImageTwo);
             this.Controls.Add(this.profileImageOne);
             this.Controls.Add(this.selectImageText);
-            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "AddProfileImage";
             this.Text = "AddProfileImage";
             ((System.ComponentModel.ISupportInitialize)(this.profileImageFour)).EndInit();
